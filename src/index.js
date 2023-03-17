91c0a12ceffb62ae3dc76d908b7104d832e04475
ellis <lsp1@hotmail.com>
2023-03-17 17:25:17 -0600

    add navbar scroll effect

--- src/index.js
+++ src/index.js
@@ -20,3 +20,30 @@ root.append(projectsElement);
 root.append(certificatesElement);
 root.append(todosElement);
 root.append(hobbiesElement);
+
+window.addEventListener("scroll", () => {
+  navbarScrollOpacityChange();
+});
+
+const navbarScrollOpacityChange = () => {
+  let scrollY = window.pageYOffset;
+  const smallAuthClassList =
+    "container rounded my-5 d-flex flex-column d-lg-none card-opacity text-center py-1";
+  const getNavbar = document.querySelector(".navbar-menu");
+  const getAuthMessageLargeText = document.querySelector(
+    "#auth-text-large-screen"
+  );
+  const getAuthMessageSmall = document.querySelector("#auth-small-screen");
+  if (scrollY > 100) {
+    getNavbar.style.backgroundColor = "rgba(50, 131, 210, 0.93)";
+    getAuthMessageLargeText.classList.remove("text-success");
+    getAuthMessageLargeText.classList.add("text-light");
+    getAuthMessageSmall.className = "a";
+    getAuthMessageSmall.style.display = "none";
+  } else if (scrollY === 0) {
+    getNavbar.style.backgroundColor = "rgba(50, 131, 210, .25)";
+    getAuthMessageLargeText.classList.remove("text-light");
+    getAuthMessageLargeText.classList.add("text-success");
+    getAuthMessageSmall.className = smallAuthClassList;
+  }
+};
