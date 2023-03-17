5213616c52c4186b93a2c479b7880eb40bce000b
ellis <lsp1@hotmail.com>
2023-03-17 17:03:48 -0600

    index.js / elements.js import/export elements

--- src/components/elements.js
+++ src/components/elements.js
@@ -1,3 +1,16 @@
 import { aboutMeElement } from "./about-me/about-me";
 import { underConstructionElement } from "./construction/under-construction";
-export { aboutMeElement, underConstructionElement };
+import { navbarElement } from "./navbar/navbar";
+import { projectsElement } from "./projects/projects";
+import { certificatesElement } from "./certificates/certificates";
+import { todosElement } from "./Todos/todos";
+import { hobbiesElement } from "./hobbies/hobbies";
+export {
+  aboutMeElement,
+  underConstructionElement,
+  navbarElement,
+  projectsElement,
+  certificatesElement,
+  todosElement,
+  hobbiesElement,
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import "./main.scss";
 import {
+  navbarElement,
   aboutMeElement,
   underConstructionElement,
+  projectsElement,
+  certificatesElement,
+  todosElement,
+  hobbiesElement,
 } from "./components/elements";
 
 // the root div in index.html for content to be injected
 const root = document.getElementById("root");
 
+root.append(navbarElement);
 root.append(underConstructionElement);
 root.append(aboutMeElement);
+root.append(projectsElement);
+root.append(certificatesElement);
+root.append(todosElement);
+root.append(hobbiesElement);
