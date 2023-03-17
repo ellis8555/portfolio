64a3b7304df73fb56afe821d99722effca02d605
ellis <lsp1@hotmail.com>
2023-03-17 17:04:22 -0600

    create todos.js

---
+++ src/components/Todos/todos-FRAG.js
@@ -0,0 +1,32 @@
+import nodeJsIcon from "../../images/todos/node-js.jpg";
+
+const todosFRAG = `
+<p class="display-5 text-center">Todos</p>
+<h4 class="text-center my-5">-- Personal --</h4>
+<div class="row text-center rounded">
+  <div class="col">
+    <div class="card h-100 card-opacity">
+      <div class="row g-0 h-100">
+        <div class="col-md-4">
+          <img
+            src="${nodeJsIcon}"
+            class="img-fluid rounded-start h-100 w-100"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-8 align-self-center">
+          <div class="card-body">
+            <h5 class="card-title">List of things to accomplish</h5>
+            <ul class="list-group list-group-flush rounded card-ul-opacity">
+              <li class="list-group-item">1. Full stack this page</li>
+              <li class="list-group-item">2. Next.js</li>
+              <li class="list-group-item">3. React Native</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+export { todosFRAG };
---
+++ src/components/Todos/todos.js
@@ -0,0 +1,10 @@
+import { createRouteContainer } from "../../utilities/routes/createDivRoutes";
+import { todosFRAG } from "./todos-FRAG";
+
+const todos = `
+<div class="container px-5 py-3 mt-5" id="projectsContainer">
+  <div id="todoSnippets">${todosFRAG}</div>
+</div>`;
+
+const todosElement = createRouteContainer(todos, "todos");
+export { todosElement };
