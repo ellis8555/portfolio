db017792768bdbc7ef7c0134726a926d922fa140
ellis <lsp1@hotmail.com>
2023-03-19 17:38:53 -0600

    links injector method created

---
+++ src/components/navbar/navbar-methods/links-injector.js
@@ -0,0 +1,25 @@
+import { listItems } from "../navbar-list-items";
+
+const createLinkItems = (...linkItems) => {
+  const containerDiv = document.createElement("div");
+  linkItems.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("p-2");
+    li.classList.add("nav-item");
+    li.classList.add("rounded");
+    li.setAttribute("data-name", `${item.elementName}`);
+    const a = document.createElement("a");
+    a.href = "#";
+    a.classList.add("text-light");
+    a.classList.add("text-decoration-none");
+    a.classList.add("nav-link");
+    const listName = document.createTextNode(`${item.linkName}`);
+    a.append(listName);
+    li.append(a);
+    containerDiv.append(li);
+  });
+  return containerDiv;
+};
+
+const navbarLinkItems = createLinkItems(...listItems).innerHTML;
+export { navbarLinkItems };
