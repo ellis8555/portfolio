e58bbca240e74f49efbb17c6c5cf64e98619585f
ellis <lsp1@hotmail.com>
2023-03-19 10:05:58 -0600

    rename createRouteContainer as self

--- src/utilities/allUtilities.js
+++ src/utilities/allUtilities.js
@@ -1,4 +1,4 @@
 import { appendElements } from "./routes/elementsAppend";
-import { createRouteContainer } from "./routes/createDivRoutes";
+import { createRouteContainer } from "./routes/createRouteContainer";
 import { setNavBarScrollFade } from "./effects/navbarScrollOpacityChange/navbarScrollOpacityChange";
 export { createRouteContainer, appendElements, setNavBarScrollFade };
