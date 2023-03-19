4e65adc2d40ec12cf65b0ab8a7a21980f0d2a4ac
ellis <lsp1@hotmail.com>
2023-03-19 15:45:07 -0600

    aboutMeElement bottom margin added

--- src/components/about-me/about-me.js
+++ src/components/about-me/about-me.js
@@ -2,7 +2,7 @@ import { createRouteContainer } from "../../utilities/allUtilities";
 import { aboutMeFRAG } from "./about-me-FRAG";
 
 const aboutMe = `
-<div class="container pt-5 px-5">
+<div class="container py-5 px-5">
   <p class="display-5 text-center pb-5">About me</p>
   <div class="rounded">
     <div id="aboutMe">${aboutMeFRAG}</div>
