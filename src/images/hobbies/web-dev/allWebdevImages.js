261e36cb2feda0e5a77547c6f866d899d110a270
ellis <lsp1@hotmail.com>
2023-03-18 22:48:23 -0600

    allwebdevimages.js created

---
+++ src/images/hobbies/web-dev/allWebdevImages.js
@@ -0,0 +1,123 @@
+import bootstrapIcon from "./bootstrap.png";
+import css3Icon from "./css3.png";
+import expressIcon from "./express.png";
+import firebaseIcon from "./firebase.png";
+import html5Icon from "./html5.png";
+import javaIcon from "./java.png";
+import javascriptIcon from "./javascript.png";
+import jwtIcon from "./jwt.jpg";
+import mongoDBIcon from "./mongoDB.png";
+import mySqlIcon from "./mySql.png";
+import nodeJsIcon from "./nodeJs.jpg";
+import phpIcon from "./php.png";
+import postcssIcon from "./postcss.png";
+import pythonIcon from "./python.png";
+import reactIcon from "./react.png";
+import sassIcon from "./sass.png";
+import tailwindIcon from "./tailwind.png";
+import typescriptIcon from "./typescript.png";
+import webpackIcon from "./webpack.png";
+
+const eachWebdevImage = [
+  {
+    imageSrc: bootstrapIcon,
+    imageAlt: "bootstrap",
+  },
+  {
+    imageSrc: css3Icon,
+    imageAlt: "css",
+  },
+  {
+    imageSrc: expressIcon,
+    imageAlt: "express",
+  },
+  {
+    imageSrc: firebaseIcon,
+    imageAlt: "firebase",
+  },
+  {
+    imageSrc: html5Icon,
+    imageAlt: "html",
+  },
+  {
+    imageSrc: javaIcon,
+    imageAlt: "java",
+  },
+  {
+    imageSrc: javascriptIcon,
+    imageAlt: "javascript",
+  },
+  {
+    imageSrc: jwtIcon,
+    imageAlt: "jsonwebtoken",
+  },
+  {
+    imageSrc: mongoDBIcon,
+    imageAlt: "mongoDB",
+  },
+  {
+    imageSrc: nodeJsIcon,
+    imageAlt: "nodeJS",
+  },
+  {
+    imageSrc: mySqlIcon,
+    imageAlt: "mySQL",
+  },
+  {
+    imageSrc: phpIcon,
+    imageAlt: "php",
+  },
+  {
+    imageSrc: postcssIcon,
+    imageAlt: "postcss",
+  },
+  {
+    imageSrc: pythonIcon,
+    imageAlt: "python",
+  },
+  {
+    imageSrc: reactIcon,
+    imageAlt: "react",
+  },
+  {
+    imageSrc: sassIcon,
+    imageAlt: "sass",
+  },
+  {
+    imageSrc: tailwindIcon,
+    imageAlt: "tailwind",
+  },
+  {
+    imageSrc: typescriptIcon,
+    imageAlt: "typescript",
+  },
+  {
+    imageSrc: webpackIcon,
+    imageAlt: "webpack",
+  },
+];
+
+export const eachWebdevImageLength = eachWebdevImage.length;
+
+export {
+  bootstrapIcon,
+  css3Icon,
+  expressIcon,
+  firebaseIcon,
+  html5Icon,
+  javaIcon,
+  javascriptIcon,
+  jwtIcon,
+  mongoDBIcon,
+  mySqlIcon,
+  nodeJsIcon,
+  phpIcon,
+  postcssIcon,
+  pythonIcon,
+  reactIcon,
+  sassIcon,
+  tailwindIcon,
+  typescriptIcon,
+  webpackIcon,
+  eachWebdevImage,
+};
