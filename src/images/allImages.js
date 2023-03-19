08cfbbe565b37cea2970f7f39ab6150641bc4e2b
ellis <lsp1@hotmail.com>
2023-03-18 20:30:13 -0600

    import/export allimages created in images folder

---
+++ src/images/allImages.js
@@ -0,0 +1,56 @@
+import wywmJavaFundatmentals from "./WYWM-certificates/WYWM-JavaFundamentals.jpg";
+import wywmPythonFundatmentals from "./WYWM-certificates/WYWM-Python-Programming-Fundamentals.jpg";
+import wywmIntermediateDev from "./WYWM-certificates/WYWM-IntermediateWebDev.jpg";
+import wywmBigO from "./WYWM-certificates/WYWM-BigO.JPG";
+import wywmDevOps from "./WYWM-certificates/WYWM-DevOps.JPG";
+import wywmIntroDev from "./WYWM-certificates/WYWM-IntroWebDev.JPG";
+import wywmSourceControl from "./WYWM-certificates/WYWM-SourceControlFundamentals.jpg";
+import wywmPrinciplesOfProgramming from "./WYWM-certificates/WYWM-PrinciplesOfProgramming.JPG";
+import wywmQaTesting from "./WYWM-certificates/WYWM-SoftwareQATestingFundamentals.JPG";
+import wywmDigitalProjectManagement from "./WYWM-certificates/WYWM-IntroductionToDigitalProjectManagement.jpg";
+
+const eachImage = [
+  {
+    imageSrc: wywmJavaFundatmentals,
+    imageAlt: "Java fundamentals",
+  },
+  {
+    imageSrc: wywmPythonFundatmentals,
+    imageAlt: "Python fundamentals",
+  },
+  {
+    imageSrc: wywmIntermediateDev,
+    imageAlt: "Intermediate web dev",
+  },
+  {
+    imageSrc: wywmBigO,
+    imageAlt: "Big-O",
+  },
+  {
+    imageSrc: wywmDevOps,
+    imageAlt: "DevOps",
+  },
+  {
+    imageSrc: wywmIntroDev,
+    imageAlt: "Intro WebDev",
+  },
+  {
+    imageSrc: wywmSourceControl,
+    imageAlt: "Source Control",
+  },
+  {
+    imageSrc: wywmPrinciplesOfProgramming,
+    imageAlt: "Principles Of Programming",
+  },
+  {
+    imageSrc: wywmQaTesting,
+    imageAlt: "QA Testing",
+  },
+  {
+    imageSrc: wywmDigitalProjectManagement,
+    imageAlt: "Digital Project Management",
+  },
+];
+
+const WYWMCertificateLength = eachImage.length;
+export { eachImage, WYWMCertificateLength };
