ede0a83aeb8ca407f685917aed015f7a08385498
ellis <lsp1@hotmail.com>
2023-03-18 22:50:27 -0600

    webdev images and frag files now methodized

---
+++ src/components/hobbies/hobbie-FRAGS/webDev-FRAG/webDev-FRAG.js
@@ -0,0 +1,13 @@
+import { webDevImages } from "./webDev-methods/webDevImageInjector";
+
+const hobbiesWebDevFRAG = `
+<h4 class="text-center my-5">-- Web dev --</h4>
+<div class="row text-center px-5 py-3">
+  <div
+    class="col-md p-3 icons rounded web-dev-hobbies-bg d-flex flex-wrap justify-content-center gap-4"
+  >
+  ${webDevImages}
+  </div>
+</div>`;
+
+export { hobbiesWebDevFRAG };
