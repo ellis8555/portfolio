16f470af1e5eac4a108a31781865c64535313fda
ellis <lsp1@hotmail.com>
2023-03-19 17:39:15 -0600

    navbar items object array created

---
+++ src/components/navbar/navbar-list-items.js
@@ -0,0 +1,28 @@
+const listItems = [
+  {
+    elementName: "homeElement",
+    linkName: "Home",
+  },
+  {
+    elementName: "projectsElement",
+    linkName: "Projects",
+  },
+  {
+    elementName: "certificatesElement",
+    linkName: "Certificates",
+  },
+  {
+    elementName: "todosElement",
+    linkName: "Todos",
+  },
+  {
+    elementName: "hobbiesElement",
+    linkName: "Hobbies",
+  },
+  {
+    elementName: "aboutMeElement",
+    linkName: "About",
+  },
+];
+
+export { listItems };
