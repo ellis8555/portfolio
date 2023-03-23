2d9142c91ac66ed1411c7c8e35c6b4101cc70bf2
ellis <lsp1@hotmail.com>
2023-03-23 11:46:09 -0600

    add hobbiesElement to about page

--- src/components/about-me/about-me.js
+++ src/components/about-me/about-me.js
@@ -1,13 +1,16 @@
 import { createRouteContainer } from "../../utilities/allUtilities";
 import { aboutMeFRAG } from "./about-me-FRAG";
+import { hobbiesElement } from "../../components/hobbies/hobbies";
 
 const aboutMe = `
 <div class="container py-5 px-5">
   <p class="display-5 text-center pb-5">About me</p>
   <div class="rounded">
-    <div id="aboutMe">${aboutMeFRAG}</div>
+  <div id="aboutMe">${aboutMeFRAG}</div>
+  ${hobbiesElement.outerHTML}
   </div>
-</div>`;
+</div>
+`;
 
 const aboutMeElement = createRouteContainer(aboutMe, "about-me");
 export { aboutMeElement };
