8a34ddc804f0c885eed23a0c89638db350ba02ae
ellis <lsp1@hotmail.com>
2023-03-24 12:06:22 -0600

    add validation bootstrap to form

--- src/components/contact-form/contact-me-FRAGs/contact-me-FRAG.js
+++ src/components/contact-form/contact-me-FRAGs/contact-me-FRAG.js
@@ -31,12 +31,15 @@ const contactFRAG = `<p class="display-5 text-center">Contact Me</p>
   <form class="card-opacity rounded px-5" name="contactForm">
     <div class="my-3">
     <label for="name" class="form-label text-info">Name:</label>
-    <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp" />
+    <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp"/>
     <div id="nameHelp" class="form-text">Name needs to be between 2-20 characters in length</div>
+    <div id="name-invalid" class="invalid-feedback"></div>
     </div>
   <div class="mb-3">
   <label for="comment" class="form-label text-info">Comment:</label>
   <textarea class="form-control" id="comment" name="comment" placeholder="Drop me a comment or leave some feedback" rows="10"></textarea>
+  <div id="commentHelp" class="form-text">250 character limit</div>
+  <div id="comment-invalid" class="invalid-feedback"></div>
   </div>
   <button type="submit" class="btn btn-primary mb-3">Submit</button>
   </form>
