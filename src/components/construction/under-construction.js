d1c63e63a0b75a0b6ae7ba51bffcb126dbe6352d
ellis <lsp1@hotmail.com>
2023-03-24 16:14:30 -0600

    adjust margin on construction element for mobile

--- src/components/construction/under-construction.js
+++ src/components/construction/under-construction.js
@@ -2,8 +2,8 @@ import { createRouteContainer } from "../../utilities/allUtilities";
 import { underConstructionFRAG } from "./under-construction-FRAG";
 
 const underConstruction = `
-<div class="ms-5 me-5">
-  <div class="container under-construction rounded text-center px-5 pt-3">
+<div class="mx-3 mx-md-5">
+  <div class="container under-construction rounded text-center px-3 py-3 px-md-5">
     <p class="display-6 under-construction-text-color">
       Site is under construction
     </p>
