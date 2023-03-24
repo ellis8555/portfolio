820bd0e4f2140b3e2a5250a73b8f2a403b91d7e2
ellis <lsp1@hotmail.com>
2023-03-24 15:27:51 -0600

    comments added in appendSingleElement.js

--- src/utilities/routes/appendSingleElement.js
+++ src/utilities/routes/appendSingleElement.js
@@ -5,17 +5,24 @@ import { clearContactFormStyles } from "../allUtilities";
 
 const appendSingleElement = (root, elementToAppend, ...otherArgs) => {
   // if moving away from contact form then remove listener on submit button
+  // also returns boolean used further down. see comments
   const wasFormSubmitted = cleanUp();
   const navbarElement = root.firstElementChild;
+  // this grabs the div container holding the current element that has been navigated to
+  // remove this element and then append the selected navigated element to append
   const elementToRemove = navbarElement.nextElementSibling;
   elementToRemove.remove();
   root.append(elementToAppend);
+  // settimeout required for scroll to top of page to work properly
   setTimeout(() => {
     window.scrollTo(0, 0);
   }, 0);
+  // when navigating to contact form clear values and styles
   if (elementToAppend === contactElement) {
     clearContactFormStyles();
   }
+  // if navigating from contact form check to see if data was submitted
+  // if data was submitted then apply message received on home element
   if (wasFormSubmitted) {
     if (elementToAppend === homeElement) {
       if (otherArgs.length > 0) {
