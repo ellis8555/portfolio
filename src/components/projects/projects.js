d3fe6b35edebcfd08be65f33785cd551b9efd3b3
ellis <lsp1@hotmail.com>
2023-03-26 10:30:25 -0600

    projects container removed px-sm-5 added

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -3,7 +3,7 @@ import { personalFRAG } from "./project-FRAGS/personal-FRAG";
 import { wywmProjectsFrag } from "./project-FRAGS/wywm-FRAG";
 
 const projects = `
-<div class="container px-5 py-5  mt-5">
+<div class="px-sm-5 py-5  mt-5">
   <div id="projectSnippets">
     <p class="display-5 text-center">Projects</p>
     <div id="personal">${personalFRAG}</div>
