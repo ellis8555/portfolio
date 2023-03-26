9cfb94a2914e18eb94f74a32dc22b813a5ac2bef
ellis <lsp1@hotmail.com>
2023-03-26 11:57:28 -0600

    remove container class from hobbiesjs

--- src/components/hobbies/hobbies.js
+++ src/components/hobbies/hobbies.js
@@ -3,7 +3,7 @@ import { hobbiesGamesFRAG } from "./hobbie-FRAGS/games-FRAG";
 import { hobbiesWebDevFRAG } from "./hobbie-FRAGS/webDev-FRAG/webDev-FRAG";
 
 const hobbies = `
-<div class="container pt-3 pb-3 mt-5" id="enjoymentsContainer">
+<div class="pt-3 pb-3 mt-5" id="enjoymentsContainer">
   <div id="hobbiesSection">
     <p class="display-5 text-center">Hobbies</p>
     <div id="webDev">${hobbiesWebDevFRAG}</div>
