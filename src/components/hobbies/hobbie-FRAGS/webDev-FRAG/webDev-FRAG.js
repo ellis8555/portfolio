78fdc234e08a61d0022cea4443b55429669dd8ca
ellis <lsp1@hotmail.com>
2023-03-26 12:02:22 -0600

    remove px in webdev frag

--- src/components/hobbies/hobbie-FRAGS/webDev-FRAG/webDev-FRAG.js
+++ src/components/hobbies/hobbie-FRAGS/webDev-FRAG/webDev-FRAG.js
@@ -2,9 +2,9 @@ import { webDevImages } from "./webDev-methods/webDevImageInjector";
 
 const hobbiesWebDevFRAG = `
 <h4 class="text-center my-5">-- Web dev --</h4>
-<div class="row text-center px-5 py-3">
+<div class="row text-center py-3">
   <div
-    class="col-md p-3 icons rounded web-dev-hobbies-bg d-flex flex-wrap justify-content-center gap-4"
+    class="col-md icons rounded web-dev-hobbies-bg d-flex flex-wrap justify-content-center gap-4"
   >
   ${webDevImages}
   </div>
