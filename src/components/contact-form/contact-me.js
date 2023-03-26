4bf224f071eda406fba4ad605bd1298c1ee40528
ellis <lsp1@hotmail.com>
2023-03-26 10:06:07 -0600

    contactMe.js remove container add px-sm-5

--- src/components/contact-form/contact-me.js
+++ src/components/contact-form/contact-me.js
@@ -2,8 +2,8 @@ import { createRouteContainer } from "../../utilities/allUtilities";
 import { contactFRAG } from "./contact-me-FRAGs/contact-me-FRAG";
 
 const contactMe = `
-<div class="container px-5 py-3 mt-5">
-  <div id="todoSnippets">${contactFRAG}</div>
+<div class=" px-sm-5 py-3 mt-5">
+  <div id="contactMe">${contactFRAG}</div>
 </div>`;
 
 const contactElement = createRouteContainer(contactMe, "contact-me");
