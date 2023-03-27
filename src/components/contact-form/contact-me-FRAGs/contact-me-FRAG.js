e4737aa2ffa98776fe28605a41bafb311a70e961
ellis <lsp1@hotmail.com>
2023-03-26 19:13:50 -0600

    cut form html from contact-me-FRAG

--- src/components/contact-form/contact-me-FRAGs/contact-me-FRAG.js
+++ src/components/contact-form/contact-me-FRAGs/contact-me-FRAG.js
@@ -1,6 +1,6 @@
+import { contactFormElement } from "./contact-me-form-FRAG";
+
 const contactFRAG = `<p class="display-5 text-center">Contact Me</p>
-
-
 <div class="rounded text-center d-md-inline-flex w-100 justify-content-md-around my-5 py-3 card-opacity">
   <p class="text-info h5 font-weight-bold">
     <svg
@@ -28,28 +28,7 @@ const contactFRAG = `<p class="display-5 text-center">Contact Me</p>
 <h4 class="text-center">Leave a comment or feedback</h4>
 
 <div class="rounded d-flex justify-content-center mt-5">
-  <form class="container-fluid card-opacity rounded px-sm-5" name="contactForm">
-    <div class="my-3">
-      <label for="name" class="form-label text-info">Name:</label>
-      <div class="form-group row">
-        <div class="col-md-4">
-  <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp"/>
-        </div>
-        <div class="col-md-8">
-    <div id="nameHelp" class="form-text">Name needs to be between 2-20 characters in length</div>
-        </div>
-      </div>
-
-    <div id="name-invalid" class="invalid-feedback"></div>
-    </div>
-  <div class="mb-3">
-  <label for="comment" class="form-label text-info">Comment:</label>
-  <textarea class="form-control" id="comment" name="comment" placeholder="Drop me a comment or leave some feedback" rows="10"></textarea>
-  <div id="commentHelp" class="form-text">250 character limit</div>
-  <div id="comment-invalid" class="invalid-feedback"></div>
-  </div>
-  <button type="submit" class="btn btn-primary mb-3">Submit</button>
-  </form>
+  ${contactFormElement.outerHTML}
 </div>`;
 
 export { contactFRAG };
