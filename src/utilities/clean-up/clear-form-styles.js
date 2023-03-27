659d94e7a682fdea1855b997a805855c1ed4e396
ellis <lsp1@hotmail.com>
2023-03-26 22:06:38 -0600

    remove validation styles after form submitted

--- src/utilities/clean-up/clear-form-styles.js
+++ src/utilities/clean-up/clear-form-styles.js
@@ -1,15 +1,21 @@
+// method used in appendSingElement.js
+// when a user submits a form they are redirected back to home element
+// if the user navigates back to the contact element this method is run
+
 const clearContactFormStyles = () => {
   const form = document.forms.contactForm;
   form.reset();
+  const submitBtn = document.elements.submit;
+  submitBtn.disabled = true;
 
   const nameInput = form.elements.name;
   const commentInput = form.elements.comment;
   const nameErrorDiv = document.getElementById("name-invalid");
   const commentErrorDiv = document.getElementById("comment-invalid");
 
-  nameInput.classList.remove("is-invalid");
+  nameInput.classList.remove("is-invalid", "is-valid");
   nameErrorDiv.innerHTML = "";
-  commentInput.classList.remove("is-invalid");
+  commentInput.classList.remove("is-invalid", "is-valid");
   commentErrorDiv.innerHTML = "";
 };
 
