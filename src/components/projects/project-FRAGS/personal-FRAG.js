9e56b3013c263f624e9352c2519a9175c4581665
ellis <lsp1@hotmail.com>
2023-03-28 15:52:46 -0600

    projects personal FRAG import components

--- src/components/projects/project-FRAGS/personal-FRAG.js
+++ src/components/projects/project-FRAGS/personal-FRAG.js
@@ -1,43 +1,9 @@
-import hax94 from "../../../images/projects/hax94.JPG";
+import { hax94Project } from "./personal-projects/hax94";
 
 const personalFRAG = `
 <h4 class="text-center my-5">-- Personal --</h4>
-<div class="row text-center rounded">
-  <div class="col">
-    <div class="card h-100 card-opacity">
-      <div class="row g-0 h-100">
-        <div class="col-md-4">
-          <img
-            src="${hax94}"
-            class="img-fluid rounded-start h-100 w-100"
-            alt="..."
-          />
-        </div>
-        <div class="col-md-8 align-self-center">
-          <div class="card-body">
-            <h5 class="card-title">League website</h5>
-            <p class="card-text">
-              A project for a 3v3 league that provides standings, stats and
-              replays! <br />
-              Over time this project will continuously improve in many different
-              ways!
-            </p>           
-              <small class="text-muted"
-                ><a href="https://hax94.com/" class="btn btn-primary"
-                  >Hax94</a
-                ></small
-              >
-              <small class="text-muted"
-                ><a href="https://github.com/ellis8555/Apple-Jack/" class="btn btn-primary"
-                  >Github</a
-                ></small
-              >   
-              <p class="mt-2 card-text">Project details on github readme</p>        
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>`;
+<!-- hax94 league -->
+${hax94Project}
+`;
 
 export { personalFRAG };
