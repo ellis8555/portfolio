de81946fb95b0552f70fe49adfb9f373c5100f13
ellis <lsp1@hotmail.com>
2023-03-28 10:36:07 -0600

    homeFRAG add ul>li center items

--- src/components/home/home-FRAG/homeFRAG.js
+++ src/components/home/home-FRAG/homeFRAG.js
@@ -3,13 +3,26 @@ import { underConstructionElement } from "../../allElements";
 const homeFRAG = `
 <div class="py-5">
 ${underConstructionElement.outerHTML}
-<div class="border border-success rounded mx-3 px-3 mx-md-5 px-md-5 mt-5 py-3">
-<p class="text-justify">
+<div class="mx-3 mx-md-5 mt-5 px-3">
+<div class="d-md-flex justify-content-center">
+<ul class="list-unstyled mt-3 border border-success rounded p-3">
+<li>
+<p>
   Currently working on creating a unique portfolio project
 </p>
-<p class="text-justify">First version - HTML, javascript, CSS & bootstrap ( current )</p>
-<p class="m-0 text-justify">Second version - React & Tailwind</p>
+</li>
+<li>
+<p>This is a vanilla single page app that uses DOM injecting into a root div</p>
+</li>
+<li>
+<p>This page constructed using HTML, javascript, CSS, Sass & bootstrap</p>
+</li>
+<li>
+<p class="mb-0">Second version - React & Tailwind</p>
+</li>
+</ul>
 </div>
-</div>`;
+</div>
+`;
 
 export { homeFRAG };
