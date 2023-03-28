7fc2e85e38c06ecb25b8eb0b283b90bbfa21d655
ellis <lsp1@hotmail.com>
2023-03-28 10:35:26 -0600

    remove container class underConstruction FRAG

--- src/components/construction/under-construction.js
+++ src/components/construction/under-construction.js
@@ -2,8 +2,8 @@ import { createRouteContainer } from "../../utilities/allUtilities";
 import { underConstructionFRAG } from "./under-construction-FRAG";
 
 const underConstruction = `
-<div class=" mx-md-5">
-  <div class="container under-construction rounded text-center py-3 px-lg-3">
+<div class="mx-md-5">
+  <div class="under-construction rounded text-center py-3 overflow-hidden">
     <p class="display-6 under-construction-text-color">
       Site is under construction
     </p>
