478a364b487e2f24cc29d16e6821af1ebe050d9b
ellis <lsp1@hotmail.com>
2023-03-28 15:54:24 -0600

    create coding knights component

---
+++ src/components/projects/project-FRAGS/wywm-projects/coding-knights.js
@@ -0,0 +1,41 @@
+import wywmLogoIcon from "../../../../images/projects/wywmLogo.jpg";
+
+const codingKnights = `
+<div class="row text-center rounded mt-3">
+  <div class="col mb-3">
+    <div class="card h-100 card-opacity">
+      <div class="row g-0 h-100">
+        <div class="col-md-4">
+          <img
+            src="${wywmLogoIcon}"
+            class="img-fluid rounded-start h-100 w-100"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-8 align-self-center">
+          <div class="card-body">
+            <h5 class="card-title mb-2">
+              Python program that demonstrates CRUD OOP
+            </h5>
+            <h6 class="card-subtitle text-success text mb-2">Coding Knights</h6>
+            <p class="card-text mb-2">
+              Basic python allowing user to perform CRUD operations on objects
+            </p>
+            <p class="card-text">
+              <small class="text-muted"
+                ><a
+                  href="https://github.com/ellis8555/WYWM-CodingKnights"
+                  class="btn btn-primary"
+                  >Python project source code</a
+                ></small
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+export { codingKnights };
