f6d756e0c86a68c6409c1f797afbe8f3325d8d55
ellis <lsp1@hotmail.com>
2023-03-28 15:53:59 -0600

    create github component

---
+++ src/components/projects/project-FRAGS/github/github.js
@@ -0,0 +1,37 @@
+import githubIcon from "../../../../images/projects/gitHub.jpg";
+
+const gitHubAccount = `
+<div class="row text-center rounded mt-3">
+  <div class="col">
+    <div class="card h-100 card-opacity">
+      <div class="row g-0 h-100">
+        <div class="col-md-4">
+          <img
+            src="${githubIcon}"
+            class="img-fluid rounded-start h-100 w-100"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-8 align-self-center">
+          <div class="card-body">
+            <h5 class="card-title">My github account</h5>
+            <p class="card-text">
+              Projects that I currently have published to my account plus more
+              coming!
+            </p>
+            <p class="card-text">
+              <small class="text-muted"
+                ><a href="https://github.com/ellis8555" class="btn btn-primary"
+                  >My github</a
+                ></small
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+export { gitHubAccount };
