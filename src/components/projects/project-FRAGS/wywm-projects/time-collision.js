20825e226fd43f6151543e1688b1ffff89c2d561
ellis <lsp1@hotmail.com>
2023-03-28 15:54:58 -0600

    create time collision component

---
+++ src/components/projects/project-FRAGS/wywm-projects/time-collision.js
@@ -0,0 +1,38 @@
+import wywmLogoIcon from "../../../../images/projects/wywmLogo.jpg";
+
+const timeCollision = `
+<div class="row text-center rounded mt-3">
+  <div class="col mb-3">
+    <div class="card h-100 card-opacity">
+      <div class="row g-0 h-100">
+        <div class="col-md-4">
+          <img
+            src="${wywmLogoIcon}"
+            class="img-fluid rounded-start h-100 w-100"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-8 align-self-center">
+          <div class="card-body">
+            <h5 class="card-title mb-2">Java text turn based game</h5>
+            <h6 class="card-subtitle text-success text mb-2">Time Collision</h6>
+            <p class="card-text mb-2">
+              Basic java demonstrating OOP, looping and file organization
+            </p>
+              <small class="text-muted"
+                ><a
+                  href="https://github.com/ellis8555/time_collision"
+                  class="btn btn-primary"
+                  >Java game source code</a
+                ></small
+              >
+            <p class="card-text mt-2">Project details on github readme</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+export { timeCollision };
