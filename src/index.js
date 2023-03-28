780014ad50cdfc38bf962498985306eaeee03e6a
ellis <lsp1@hotmail.com>
2023-03-28 16:16:55 -0600

    index.js remove routeChange/setNavbarScrollFade

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import "./main.scss";
 import * as bootstrap from "bootstrap";
 import { navbarElement, homeElement } from "./components/allElements";
-import {
-  setNavBarScrollFade,
-  pageStartup,
-  routeChange,
-} from "./utilities/allUtilities";
+import { pageStartup } from "./utilities/allUtilities";
 
 window.addEventListener("DOMContentLoaded", () => {
   // the root div in index.html for content to be injected
   const root = document.getElementById("root");
 
-  // apply listeners
-
   // intial page start up to load the navbar and home elements
   pageStartup(root, navbarElement, homeElement);
-
-  // apply click listeners on the nav bar list items
-  routeChange();
-
-  // sets the scroll effect on the navbar
-  setNavBarScrollFade();
 });
