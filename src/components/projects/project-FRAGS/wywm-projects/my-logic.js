47eb97a7105a4fd615629712344cde29f452fa66
ellis <lsp1@hotmail.com>
2023-03-28 15:54:34 -0600

    create my-logic component

---
+++ src/components/projects/project-FRAGS/wywm-projects/my-logic.js
@@ -0,0 +1,36 @@
+import wywmLogoIcon from "../../../../images/projects/wywmLogo.jpg";
+
+const myGoblinStore = `
+<div class="col-md-6">
+<div class="card h-100 card-opacity">
+  <div class="row g-0 h-100">
+    <div class="col-md-4">
+      <img
+        src="${wywmLogoIcon}"
+        class="img-fluid rounded-start h-100 w-100"
+        alt="..."
+      />
+    </div>
+    <div class="col-md-8 align-self-center">
+      <div class="card-body">
+        <h5 class="card-title">My logic</h5>
+        <p class="card-text">
+          My added logic built upon the project template I was given
+        </p>
+        <p class="card-text">
+          <small class="text-muted"
+            ><a
+              href="https://ellis8555.github.io/WYWM-My-GoblinGames-Shop/"
+              class="btn btn-primary"
+              >My store project</a
+            ></small
+          >
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+`;
+
+export { myGoblinStore };
