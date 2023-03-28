b58079f8d726d71a02f19c0dfab12f8c3ace4f4e
ellis <lsp1@hotmail.com>
2023-03-28 15:54:49 -0600

    create project template component

---
+++ src/components/projects/project-FRAGS/wywm-projects/project-template.js
@@ -0,0 +1,35 @@
+import wywmLogoIcon from "../../../../images/projects/wywmLogo.jpg";
+
+const goblinStoreTemplate = `
+<div class="col-md-6">
+<div class="card mb-3 h-100 card-opacity">
+  <div class="row g-0 h-100">
+    <div class="col-md-4">
+      <img
+        src="${wywmLogoIcon}"
+        class="img-fluid rounded-start h-100 w-100"
+        alt="..."
+      />
+    </div>
+    <div class="col-md-8 align-self-center">
+      <div class="card-body">
+        <h5 class="card-title">Project Template</h5>
+        <p class="card-text">Project template I was given</p>
+        <p class="card-text">
+          <small class="text-muted"
+            ><a
+              href="https://ellis8555.github.io/The-Gobblin-Store-TEMPLATE/"
+              class="btn btn-warning"
+              >Project template</a
+            ></small
+          >
+        </p>
+        <p>See 'My logic' project for my custom expansion of this template</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+`;
+
+export { goblinStoreTemplate };
