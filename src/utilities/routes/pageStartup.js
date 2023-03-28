5bb25b14b5d5a4af703ad00b45945e823c5bf2d5
ellis <lsp1@hotmail.com>
2023-03-28 16:17:30 -0600

    pageStartup.js import routeChange/navbarfade()

--- src/utilities/routes/pageStartup.js
+++ src/utilities/routes/pageStartup.js
@@ -1,7 +1,13 @@
+import { routeChange, setNavBarScrollFade } from "../allUtilities";
+
 const pageStartup = (root, ...startElements) => {
   startElements.forEach((element) => {
     root.append(element);
   });
+  // set appropriate click listeners on navbar items
+  routeChange();
+  // sets the scroll effect on the navbar
+  setNavBarScrollFade();
 };
 
 export { pageStartup };
