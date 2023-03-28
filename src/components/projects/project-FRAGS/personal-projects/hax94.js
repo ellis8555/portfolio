1fc1be11f5c606c38100281165d86fe0f4ea831f
ellis <lsp1@hotmail.com>
2023-03-28 15:54:07 -0600

    create hax94 component

---
+++ src/components/projects/project-FRAGS/personal-projects/hax94.js
@@ -0,0 +1,43 @@
+import hax94 from "../../../../images/projects/hax94.JPG";
+
+const hax94Project = `
+<div class="row text-center rounded">
+  <div class="col">
+    <div class="card h-100 card-opacity">
+      <div class="row g-0 h-100">
+        <div class="col-md-4">
+          <img
+            src="${hax94}"
+            class="img-fluid rounded-start h-100 w-100"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-8 align-self-center">
+          <div class="card-body">
+            <h5 class="card-title">League website</h5>
+            <p class="card-text">
+              A project for a 3v3 league that provides standings, stats and
+              replays! <br />
+              Over time this project will continuously improve in many different
+              ways!
+            </p>           
+              <small class="text-muted"
+                ><a href="https://hax94.com/" class="btn btn-primary"
+                  >Hax94</a
+                ></small
+              >
+              <small class="text-muted"
+                ><a href="https://github.com/ellis8555/Apple-Jack/" class="btn btn-primary"
+                  >Github</a
+                ></small
+              >   
+              <p class="mt-2 card-text">Project details on github readme</p>        
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+export { hax94Project };
