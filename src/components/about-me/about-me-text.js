d7b7b3368baea5df6571eeaa10aabe732e0831d3
ellis <lsp1@hotmail.com>
2023-03-29 08:06:01 -0600

    edit about me points

--- src/components/about-me/about-me-text.js
+++ src/components/about-me/about-me-text.js
@@ -3,11 +3,11 @@ import { listItemInjector } from "./about-me-methods/about-me-list-injector";
 // array items will appear as text listed in about me
 const aboutMeItems = [
   "I have a passion for coding!",
-  "I am currently working towards becoming a full stack web developer.",
-  "I have a strong foundation in HTML, CSS, and JavaScript",
-  "Good solid base knowledge of front side technologies",
-  "Good solid base knowledge of server side technologies",
-  "Very adaptable and vesatile with change",
+  "I find I really enjoy javascript and java",
+  "I have a strong foundation in HTML, CSS, and javascript",
+  "Good solid base knowledge of front and back side technologies",
+  "Have experience with SQL and noSQL db's",
+  "Am always aware of the latest frameworks and willing to learn anything new",
 ];
 
 // method creates list/text nodes and extract contents using innerHTML
