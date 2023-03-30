0c8e421a629799dd3cfb5a82852069b62073b1ba
ellis <lsp1@hotmail.com>
2023-03-29 21:37:24 -0600

    links-injector takes in login/sign up

--- src/components/navbar/navbar-methods/links-injector.js
+++ src/components/navbar/navbar-methods/links-injector.js
@@ -3,16 +3,24 @@ import { listItems } from "../navbar-list-items";
 const createLinkItems = (...linkItems) => {
   const containerDiv = document.createElement("div");
   linkItems.forEach((item) => {
+    // enclosing li element
     const li = document.createElement("li");
-    li.classList.add("p-2");
-    li.classList.add("nav-item");
-    li.classList.add("rounded");
+    li.classList.add("ps-3", "px-md-1", "py-2", "nav-item", "rounded");
+    // align right login/sign up
+    if (item.marginAuto) {
+      li.classList.add(`${item.marginAuto}`);
+    }
     li.setAttribute("data-name", `${item.elementName}`);
+
+    // inner a element
     const a = document.createElement("a");
     a.href = `#${item.linkName}`;
-    a.classList.add("text-light");
-    a.classList.add("text-decoration-none");
-    a.classList.add("nav-link");
+    // assign different color to login/sign up
+    if (item.color) {
+      a.classList.add(`${item.color}`, "text-decoration-none", "nav-link");
+    } else {
+      a.classList.add("text-light", "text-decoration-none", "nav-link");
+    }
     const listName = document.createTextNode(`${item.linkName}`);
     a.append(listName);
     li.append(a);
