afc1afc540dc4056c903d740f45fdb09a44cf4fd
ellis <lsp1@hotmail.com>
2023-03-30 08:55:09 -0600

    import singupformelement.js in singup.js

--- src/components/auth/signup/signup.js
+++ src/components/auth/signup/signup.js
@@ -1,12 +1,13 @@
 import { createRouteContainer } from "../../../utilities/allUtilities";
+import { signupFormElement } from "./signupFRAGS/signup-form-FRAG";
 
 const signup = `
-<div class="px-sm-5">
+<div class="px-sm-5 py-3 mt-5">
 <p class="display-5 text-center pb-5">Sign up</p>
-<div class="rounded">
-    <div id="loginContent">
-        <h1 class="text-warning text-center"> sign up under construction</h1>
-    </div>
+<div class="row d-flex justify-content-center">
+<div class="col-sm-9 col-md-8 col-lg-6 col-xl-4 rounded d-flex justify-content-center mt-5">
+${signupFormElement.innerHTML}
+</div>
 </div>
 </div>
 `;
