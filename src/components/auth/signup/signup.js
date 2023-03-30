fa41509882a6075e31617ff955409e5327c25cad
ellis <lsp1@hotmail.com>
2023-03-30 15:49:21 -0600

    signup.js outer styling and flex classes

--- src/components/auth/signup/signup.js
+++ src/components/auth/signup/signup.js
@@ -3,9 +3,10 @@ import { signupFormElement } from "./signupFRAGS/signup-form-FRAG";
 
 const signup = `
 <div class="px-sm-5 py-3 mt-5">
-<p class="display-5 text-center pb-5">Sign up</p>
+<p class="display-5 text-center">Sign up</p>
+<div class="text-center mt-5" id="signupResponseIndicator"></div>
 <div class="row d-flex justify-content-center">
-<div class="col-sm-9 col-md-8 col-lg-6 col-xl-4 rounded d-flex justify-content-center mt-5">
+<div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 rounded d-flex justify-content-center mt-3">
 ${signupFormElement.innerHTML}
 </div>
 </div>
