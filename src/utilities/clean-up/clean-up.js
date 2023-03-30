9ef1e3e3e69c93462ca7c755d7777ce4905f4498
ellis <lsp1@hotmail.com>
2023-03-30 15:51:32 -0600

    remove listener cleanup.js signupform

--- src/utilities/clean-up/clean-up.js
+++ src/utilities/clean-up/clean-up.js
@@ -1,9 +1,14 @@
-import { getContactMessage } from "../allUtilities";
+import { getContactMessage, getSignupFormDetails } from "../allUtilities";
 
 const cleanUp = () => {
   const contactForm = document.forms.contactForm;
+  const signupForm = document.forms.signupForm;
   if (contactForm) {
     contactForm.removeEventListener("submit", getContactMessage);
+    return true;
+  }
+  if (signupForm) {
+    signupForm.removeEventListener("submit", getSignupFormDetails);
     return true;
   }
   return false;
