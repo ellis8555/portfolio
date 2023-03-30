96e579a11c200b58cb78211a127d8230a51b0951
ellis <lsp1@hotmail.com>
2023-03-30 15:50:42 -0600

    add signup utils to allutilities

--- src/utilities/allUtilities.js
+++ src/utilities/allUtilities.js
@@ -8,13 +8,22 @@ import {
   getContactMessage,
   submitContactMe,
 } from "./user-input/submit-contact-form";
+import {
+  getSignupFormDetails,
+  submitSignupForm,
+} from "./user-input/submit-signup-form";
 import { cleanUp } from "./clean-up/clean-up";
 import { clearContactFormStyles } from "./clean-up/clear-form-styles";
+import { clearsignupFormStyles } from "./clean-up/clear-signup-styles";
 import { userNameValidator } from "./user-input/validation/valiation-tests/contact-comment-input";
 import { userCommentValidator } from "./user-input/validation/valiation-tests/contact-name-input";
+import { singupUsernameValidator } from "./user-input/validation/valiation-tests/signup-name-input";
+import { singupPasswordValidator } from "./user-input/validation/valiation-tests/signup-password-input";
 import { contactFormValidationTest } from "./user-input/validation/methods/validate-contact-form";
+import { signupFormValidationTest } from "./user-input/validation/methods/validate-signup-form";
 import { loadResponseSpinner } from "./user-input/validation/methods/load-response-spinner";
 import { sanitizeComment } from "./clean-up/sanitize-text-input";
+
 export {
   createRouteContainer,
   appendAllElements,
@@ -24,11 +33,17 @@ export {
   routeChange,
   getContactMessage,
   submitContactMe,
+  getSignupFormDetails,
+  submitSignupForm,
   cleanUp,
   clearContactFormStyles,
+  clearsignupFormStyles,
   userNameValidator,
   userCommentValidator,
+  singupUsernameValidator,
+  singupPasswordValidator,
   contactFormValidationTest,
+  signupFormValidationTest,
   loadResponseSpinner,
   sanitizeComment,
 };
