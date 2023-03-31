c7a7000671ab51b06596c45faa03f99ab2456523
ellis <lsp1@hotmail.com>
2023-03-31 07:42:42 -0600

    add login methods to all utilities

--- src/utilities/allUtilities.js
+++ src/utilities/allUtilities.js
@@ -12,15 +12,21 @@ import {
   getSignupFormDetails,
   submitSignupForm,
 } from "./user-input/submit-signup-form";
+import {
+  getLoginFormDetails,
+  submitLoginForm,
+} from "./user-input/submit-login-form";
 import { cleanUp } from "./clean-up/clean-up";
 import { clearContactFormStyles } from "./clean-up/clear-form-styles";
 import { clearsignupFormStyles } from "./clean-up/clear-signup-styles";
+import { clearLoginFormStyles } from "./clean-up/clear-login-styles";
 import { userNameValidator } from "./user-input/validation/valiation-tests/contact-comment-input";
 import { userCommentValidator } from "./user-input/validation/valiation-tests/contact-name-input";
 import { singupUsernameValidator } from "./user-input/validation/valiation-tests/signup-name-input";
 import { singupPasswordValidator } from "./user-input/validation/valiation-tests/signup-password-input";
 import { contactFormValidationTest } from "./user-input/validation/methods/validate-contact-form";
 import { signupFormValidationTest } from "./user-input/validation/methods/validate-signup-form";
+import { loginFormValidationTest } from "./user-input/validation/methods/validate-login-form";
 import { loadResponseSpinner } from "./user-input/validation/methods/load-response-spinner";
 import { sanitizeComment } from "./clean-up/sanitize-text-input";
 
@@ -35,15 +41,19 @@ export {
   submitContactMe,
   getSignupFormDetails,
   submitSignupForm,
+  getLoginFormDetails,
+  submitLoginForm,
   cleanUp,
   clearContactFormStyles,
   clearsignupFormStyles,
+  clearLoginFormStyles,
   userNameValidator,
   userCommentValidator,
   singupUsernameValidator,
   singupPasswordValidator,
   contactFormValidationTest,
   signupFormValidationTest,
+  loginFormValidationTest,
   loadResponseSpinner,
   sanitizeComment,
 };
