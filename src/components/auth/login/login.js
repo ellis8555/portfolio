2612040c9f509f8014ce3f8d8829165a9eb0cf0e
ellis <lsp1@hotmail.com>
2023-03-30 18:04:47 -0600

    create login.js

--- src/components/auth/login/login.js
+++ src/components/auth/login/login.js
@@ -4,10 +4,11 @@ import { loginFormElement } from "./loginFRAGS/login-form-FRAG";
 const login = `
 <div class="px-sm-5 py-3 mt-5">
 <p class="display-5 text-center">Login</p>
-<div class="rounded card-opacity">
-    <div id="loginContent">
-        ${loginFormElement}
-    </div>
+<div class="text-center mt-5" id="loginResponseIndicator"></div>
+<div class="row d-flex justify-content-center">
+<div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 rounded d-flex justify-content-center mt-3">
+${loginFormElement.innerHTML}
+</div>
 </div>
 </div>
 `;
