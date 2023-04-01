73e010e345cca0a00069847d343d1172a740c0c9
ellis <lsp1@hotmail.com>
2023-03-31 19:01:29 -0600

    add submit() to submitloginform

--- src/utilities/user-input/submit-login-form.js
+++ src/utilities/user-input/submit-login-form.js
@@ -2,6 +2,7 @@ import {
   appendSingleElement,
   loadResponseSpinner,
   sanitizeComment,
+  login,
 } from "../allUtilities";
 import { homeElement } from "../../components/allElements";
 
@@ -42,6 +43,7 @@ const getLoginFormDetails = async (e) => {
     const responseName = result.username;
     form.reset();
     appendSingleElement(root, homeElement, responseMessage, responseName);
+    login();
   } else {
     if (result.hasOwnProperty("field")) {
       const fieldName = result.field;
