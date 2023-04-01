15db5a492fab370de7022c1adf5eb0a49681c954
ellis <lsp1@hotmail.com>
2023-03-31 19:02:22 -0600

    create login.js

---
+++ src/utilities/auth/login.js
@@ -0,0 +1,17 @@
+import { isLoggedIn } from "../allUtilities";
+
+const login = () => {
+  const username = isLoggedIn().username;
+
+  const usernameDisplayDiv = document.querySelector("#loggedInUsername");
+  const logoutLink = document.querySelector("[data-name='logoutElement']");
+  const loginLink = document.querySelector("[data-name='loginElement']");
+  const signupLink = document.querySelector("[data-name='signupElement']");
+
+  usernameDisplayDiv.innerHTML = username;
+  logoutLink.classList.remove("d-none");
+  loginLink.classList.add("d-none");
+  signupLink.classList.add("d-none");
+};
+
+export { login };
