9a17dbe148ce4e63ac9261fc97461beea7b4933c
ellis <lsp1@hotmail.com>
2023-04-03 08:13:51 -0600

    signup password regex special chars updated

--- src/utilities/user-input/validation/regex/regex-patterns.js
+++ src/utilities/user-input/validation/regex/regex-patterns.js
@@ -3,6 +3,6 @@
 const contactFormName = /^(?=[a-zA-Z ]{2,30}$)[a-zA-Z]* ?[a-zA-Z]+$/;
 const contactFormComment = /^.{8,250}$/;
 const signupFormPassword =
-  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&\-+^()[\]{}|~])[A-Za-z\d@$!%*?&\-+^()[\]{}|~]{8,}$/;
 
 export { contactFormName, contactFormComment, signupFormPassword };
