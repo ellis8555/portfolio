e764f4487f15e770af91d351d998a7c31e521a87
ellis <lsp1@hotmail.com>
2023-04-02 21:22:01 -0600

    home page construction message changed

--- src/components/construction/under-construction.js
+++ src/components/construction/under-construction.js
@@ -4,15 +4,9 @@ import { underConstructionFRAG } from "./under-construction-FRAG";
 const underConstruction = `
 <div class="mx-md-5">
   <div class="under-construction rounded text-center py-3 overflow-hidden">
-    <p class="display-6 under-construction-text-color">
-      Site is under construction
+    <p class="fs-4 under-construction-text-color">
+      Going to begin the <span class="text-info">upcoming version</span> of this project
     </p>
-    <div class="row">
-    <div class="bg-success rounded fs-5 mb-3 mx-auto col-md-8">Login, Logout, Sign up and contact form all functional.</div>
-    </div>
-    <div class="row">
-    <div class="bg-primary rounded fs-5 mb-3 mx-auto col-md-8">Accounts only created for the time being. Setting up authorized endpoints will come in the future</div>
-    </div>
     <div id="construction-message">${underConstructionFRAG}</div>
   </div>
 </div>`;
