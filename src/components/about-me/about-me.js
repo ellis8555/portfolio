5340c5d3d6c2bafbc41cc0dd5d5b683fc10ced13
ellis <lsp1@hotmail.com>
2023-04-04 09:11:36 -0600

    adjust h4 tags in aboutMe

--- src/components/about-me/about-me.js
+++ src/components/about-me/about-me.js
@@ -7,8 +7,9 @@ const aboutMe = `
 <div class="px-sm-5 py-5">
   <p class="display-5 text-center pb-5">About me</p>
   <!-- my github -->
-<h4 class="text-center my-5">-- Github --</h4>
+<h4 class="text-center mb-5">-- Github --</h4>
 ${gitHubAccount}
+<h4 class="text-center my-5">-- Myself --</h4>
   <div class="rounded mt-5">
   <div id="aboutMe">${aboutMeFRAG}</div>
   ${hobbiesElement.outerHTML}
