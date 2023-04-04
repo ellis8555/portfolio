9260ccd3eb7e36501e4fab13e035b6acde414594
ellis <lsp1@hotmail.com>
2023-04-04 09:05:37 -0600

    add my github link to aboutMe

--- src/components/about-me/about-me.js
+++ src/components/about-me/about-me.js
@@ -1,11 +1,15 @@
 import { createRouteContainer } from "../../utilities/allUtilities";
 import { aboutMeFRAG } from "./about-me-FRAG";
 import { hobbiesElement } from "../../components/hobbies/hobbies";
+import { gitHubAccount } from "../projects/project-FRAGS/github/github";
 
 const aboutMe = `
 <div class="px-sm-5 py-5">
   <p class="display-5 text-center pb-5">About me</p>
-  <div class="rounded">
+  <!-- my github -->
+<h4 class="text-center my-5">-- Github --</h4>
+${gitHubAccount}
+  <div class="rounded mt-5">
   <div id="aboutMe">${aboutMeFRAG}</div>
   ${hobbiesElement.outerHTML}
   </div>
