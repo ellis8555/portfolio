5ddcaa5aa6ced3f58a1ff787218ab0f777e55917
ellis <lsp1@hotmail.com>
2023-04-05 09:14:17 -0600

    display username on xs and sm screens

--- src/utilities/routes/pageStartup.js
+++ src/utilities/routes/pageStartup.js
@@ -9,8 +9,12 @@ const pageStartup = (root, ...startElements) => {
   const isUserLoggedIn = isLoggedIn().isSignedIn;
   if (isUserLoggedIn) {
     const username = isLoggedIn().user.username;
+    // display user name on mediumn screens and larger
     const displayUsernameDiv = document.getElementById("loggedInUsername");
     displayUsernameDiv.innerHTML = username;
+    // display username on xs and small screens
+    const mobileUsernameDisplayDiv = document.querySelector("#mobileLoggedIn");
+    mobileUsernameDisplayDiv.innerHTML = username;
   }
 
   // set appropriate click listeners on navbar items
