59defa39e2d642e803a80a4b2d9fc51e8f433d10
ellis <lsp1@hotmail.com>
2023-04-05 08:52:41 -0600

    add pt-5 to home element

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,8 +1,8 @@
 import { createRouteContainer } from "../../utilities/allUtilities";
 import { homeFRAG } from "./home-FRAG/homeFRAG";
 
-const index = `<div class="px-sm-5">
-<p class="display-5 text-center pb-5">My portfolio</p>
+const index = `<div class="mt-5 px-sm-5">
+<p class="display-5 text-center mb-5">My portfolio</p>
 <div class="rounded card-opacity">
     <div id="homeContent">${homeFRAG}</div>
 </div>
