82c1f3df2820f6a802daad8453616cea191e10f9
ellis <lsp1@hotmail.com>
2023-04-05 09:15:08 -0600

    display username login xs and sm screens

--- src/utilities/auth/login.js
+++ src/utilities/auth/login.js
@@ -4,11 +4,13 @@ const login = () => {
   const username = isLoggedIn().user.username;
 
   const usernameDisplayDiv = document.querySelector("#loggedInUsername");
+  const mobileUsernameDisplayDiv = document.querySelector("#mobileLoggedIn");
   const logoutLink = document.querySelector("[data-name='logoutElement']");
   const loginLink = document.querySelector("[data-name='loginElement']");
   const signupLink = document.querySelector("[data-name='signupElement']");
 
   usernameDisplayDiv.innerHTML = username;
+  mobileUsernameDisplayDiv.innerHTML = username;
   logoutLink.classList.remove("d-none");
   loginLink.classList.add("d-none");
   signupLink.classList.add("d-none");
