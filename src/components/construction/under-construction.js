b2bf58105030a00a6488a67f2c59d9ff84869adb
ellis <lsp1@hotmail.com>
2023-04-17 09:49:59 -0600

    react button link added

--- src/components/construction/under-construction.js
+++ src/components/construction/under-construction.js
@@ -5,9 +5,9 @@ const underConstruction = `
 <div class="mx-md-5">
   <div class="under-construction rounded text-center py-3 overflow-hidden">
   <p class="fs-3 under-construction-text-color">This Vanilla JS version - <span class="fs-6">100% functional</span></p>
-    <p class="fs-4 under-construction-text-color text-decoration-underline">
-    <a href="https://dreamy-cocada-3ca0d7.netlify.app" class="text-info">React version of this project<span class="fs-6 under-construction-text-color"> - under construction</span></a>
-    </p>
+
+    <a href="https://dreamy-cocada-3ca0d7.netlify.app" class="btn btn-info text-light mb-3">React version</a>
+  <p>React version is under construction</p>
     <div id="construction-message">${underConstructionFRAG}</div>
   </div>
 </div>`;
