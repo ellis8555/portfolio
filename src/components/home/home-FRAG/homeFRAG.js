b1fa77ea77d721705c6c1b964b3677c39da735e4
ellis <lsp1@hotmail.com>
2023-04-17 09:36:21 -0600

    home message points update

--- src/components/home/home-FRAG/homeFRAG.js
+++ src/components/home/home-FRAG/homeFRAG.js
@@ -7,18 +7,13 @@ ${underConstructionElement.outerHTML}
 <div class="d-md-flex justify-content-center">
 <ul class="list-unstyled mt-3 border border-success rounded p-3">
 <li>
-<p>
-  Currently working on the react version of this portfolio
-</p>
-</li>
-<li>
 <p>This is a vanilla single page app that uses DOM injecting into a root div</p>
 </li>
 <li>
-<p>This page constructed using HTML, javascript, CSS, Sass, bootstrap & packed using webpack</p>
+<p>Custom webpack configuration used to bundle this version</p>
 </li>
 <li>
-<p class="mb-0">Second version - React & Tailwind</p>
+<p>Bootstrap is used for styling and animations in this project</p>
 </li>
 </ul>
 </div>
