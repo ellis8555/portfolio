3b6f82a9a496bc35e743dee14f1f22b44a6c1c77
ellis <lsp1@hotmail.com>
2023-05-08 12:49:37 -0600

    signup messeage edit

--- src/components/auth/signup/signup.js
+++ src/components/auth/signup/signup.js
@@ -5,7 +5,7 @@ const signup = `
 <div class="px-sm-5 py-3 mt-5">
 <p class="display-5 text-center">Sign up</p>
 <div class="row d-flex justify-content-center">
-<div class="card-opacity col-sm-12 col-md-9 col-lg-7 col-xl-6 rounded d-flex justify-content-center mt-3 px-3 py-2">Account creation is solely for demonstration purposes. Setting up authorized endpoints will come in the future</div>
+<div class="card-opacity col-sm-12 col-md-9 col-lg-7 col-xl-6 rounded d-flex justify-content-center mt-3 px-3 py-2">Account creation is solely for demonstration purposes. An account is actually created where the user can login and out.</div>
 </div>
 <div class="text-center mt-5" id="signupResponseIndicator"></div>
 <div class="row d-flex justify-content-center">
