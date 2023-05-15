2ad47a75b232ea5acf3fb803cd132756f52b3127
ellis <lsp1@hotmail.com>
2023-05-15 14:04:34 -0600

    edited message in comment input box

--- src/components/contact-form/contact-me-FRAGs/contact-me-form-FRAG.js
+++ src/components/contact-form/contact-me-FRAGs/contact-me-form-FRAG.js
@@ -17,7 +17,7 @@ const contactForm = `
 </div>
 <div class="mb-3">
 <label for="comment" class="form-label text-info">Comment:</label>
-<textarea class="form-control" id="comment" name="comment" placeholder="Drop me a comment or leave some feedback" rows="10"></textarea>
+<textarea class="form-control" id="comment" name="comment" placeholder="Email me a comment or leave some feedback here" rows="10"></textarea>
 <div id="commentHelp" class="form-text">Between 8 - 250 characters</div>
 <div id="comment-invalid" class="invalid-feedback"></div>
 </div>
