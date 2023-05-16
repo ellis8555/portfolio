5debce8f4dddbacdaed849e8c46de17dab001eaf
ellis <lsp1@hotmail.com>
2023-05-16 16:40:36 -0600

    create contact-comment-input test

---
+++ src/utilities/user-input/validation/valiation-tests/__tests__/contact-comment-input.test.js
@@ -0,0 +1,14 @@
+import { userCommentValidator } from "../contact-name-input";
+
+// invalid comment tests
+
+describe("valid comment input", () => {
+  test("less than min char length required", () => {
+    expect(userCommentValidator("1234567")).toBe(false);
+  });
+  test("over max character allowed", () => {
+    const twoHundredFiftyOneChars =
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium q";
+    expect(userCommentValidator(twoHundredFiftyOneChars)).toBe(false);
+  });
+});
