c476fa9fe9e92bd3586cbc1e8d3d0bb22ffd39cf
ellis <lsp1@hotmail.com>
2023-05-16 16:40:21 -0600

    signup-name-input test comments edit

--- src/utilities/user-input/validation/valiation-tests/__tests__/signup-name-input.test.js
+++ src/utilities/user-input/validation/valiation-tests/__tests__/signup-name-input.test.js
@@ -1,22 +1,22 @@
 import { singupUsernameValidator } from "../signup-name-input";
 
-// singupUsernameValidator
-describe("singupUsernameValidator", () => {
-  describe("various signupUsernameValidator lengths of 2 or more", () => {
-    test("returns true on 2 characters entered", () => {
-      expect(singupUsernameValidator("ab")).toBe(true);
-    });
-    test("returns true on 3 characters entered", () => {
-      expect(singupUsernameValidator("abc")).toBe(true);
-    });
-  });
+// valid user names
 
-  describe("various signupUsernameValidator lengths of less than two", () => {
-    test("returns true on 1 character entered", () => {
-      expect(singupUsernameValidator("a")).toBe(false);
-    });
-    test("returns true on 0 characters entered", () => {
-      expect(singupUsernameValidator("")).toBe(false);
-    });
+describe("various signupUsernameValidator lengths of 2 or more", () => {
+  test("returns true on 2 characters entered", () => {
+    expect(singupUsernameValidator("ab")).toBe(true);
+  });
+  test("returns true on 3 characters entered", () => {
+    expect(singupUsernameValidator("abc")).toBe(true);
+  });
+});
+
+// invalid user names
+describe("various signupUsernameValidator lengths of less than two", () => {
+  test("returns true on 1 character entered", () => {
+    expect(singupUsernameValidator("a")).toBe(false);
+  });
+  test("returns true on 0 characters entered", () => {
+    expect(singupUsernameValidator("")).toBe(false);
   });
 });
