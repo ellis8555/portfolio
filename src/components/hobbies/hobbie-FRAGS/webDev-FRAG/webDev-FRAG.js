9aa49aa66d6988c211e78f79b75cb7ca2d4049ac
ellis <lsp1@hotmail.com>
2023-05-18 09:20:28 -0600

    have used message all language icons

--- src/components/hobbies/hobbie-FRAGS/webDev-FRAG/webDev-FRAG.js
+++ src/components/hobbies/hobbie-FRAGS/webDev-FRAG/webDev-FRAG.js
@@ -2,6 +2,7 @@ import { webDevImages } from "./webDev-methods/webDevImageInjector";
 
 const hobbiesWebDevFRAG = `
 <h4 class="text-center my-5">-- Web dev --</h4>
+<p class="card-opacity text-light text-center w-50 mx-auto rounded">I have used each of and every of these</p>
 <div class="row text-center py-3">
   <div
     class="col-md icons rounded web-dev-hobbies-bg d-flex flex-wrap justify-content-center gap-4"
