73ed1604b4b78f4a5d7852ddcbd47d3947c2756e
ellis <lsp1@hotmail.com>
2023-05-19 14:17:25 -0600

    contact-me-FRAG break into another module

--- src/components/contact-form/contact-me-FRAGs/contact-me-FRAG.js
+++ src/components/contact-form/contact-me-FRAGs/contact-me-FRAG.js
@@ -1,21 +1,12 @@
 import { contactFormElement } from "./contact-me-form-FRAG";
+import { contactFragBodySnippet } from "./contact-me-snippets/contact-frag-body-snippet";
 
-const contactFRAG = `<p class="display-5 text-center">Contact Me</p>
-<div class="rounded text-center d-md-inline-flex w-100 justify-content-md-around my-5 py-3 card-opacity">
-  <p id="gmail-icon" class="text-info h5 font-weight-bold">
-    ellis8555@gmail.com
-  </p>
-  <a
-    href="https://www.linkedin.com/in/lonnie-smith-4b0583208/"
-    class="text-info h5 font-weight-bold"
-    id="linkedin-link"
-    >My linkedin</a>
-</div>
-<h4 class="text-center">Leave a comment or feedback</h4>
-<div class="text-center mt-5" id="responseIndicator">
-</div>
+const contactFormContainer = `
 <div class="rounded d-flex justify-content-center mt-5">
-  ${contactFormElement.outerHTML}
-</div>`;
+${contactFormElement.outerHTML}
+</div>
+`;
+
+const contactFRAG = contactFragBodySnippet + contactFormContainer;
 
 export { contactFRAG };
