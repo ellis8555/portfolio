f10c56922c98f44db41ac772c167b9c416c66546
ellis <lsp1@hotmail.com>
2023-05-19 14:19:07 -0600

    create contact-frag-body-snippet-test.js

---
+++ src/components/contact-form/__tests__/contact-frag-body-snippet.test.js
@@ -0,0 +1,24 @@
+import { contactFragBodySnippet } from "../contact-me-FRAGs/contact-me-snippets/contact-frag-body-snippet";
+
+import jsdomGlobal from "jsdom-global";
+import cleanup from "jsdom-global";
+import "@testing-library/jest-dom";
+
+jsdomGlobal();
+
+beforeEach(() => {
+  document.body.innerHTML = contactFragBodySnippet;
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+afterAll(() => {
+  window.close();
+});
+
+it("should display correct email", () => {
+  const email = document.getElementById("gmail-icon");
+  expect(email).toHaveTextContent(/ellis8555@gmail.com/);
+});
