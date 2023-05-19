06809fd73f4c03d9379e1d41496bd259758f7a03
ellis <lsp1@hotmail.com>
2023-05-19 14:18:39 -0600

    create contactfragbodysnippet.js

---
+++ src/components/contact-form/contact-me-FRAGs/contact-me-snippets/contact-frag-body-snippet.js
@@ -0,0 +1,17 @@
+const contactFragBodySnippet = `<p class="display-5 text-center">Contact Me</p>
+<div class="rounded text-center d-md-inline-flex w-100 justify-content-md-around my-5 py-3 card-opacity">
+  <p id="gmail-icon" class="text-info h5 font-weight-bold">
+    ellis8555@gmail.com
+  </p>
+  <a
+    href="https://www.linkedin.com/in/lonnie-smith-4b0583208/"
+    class="text-info h5 font-weight-bold"
+    id="linkedin-link"
+    >My linkedin</a>
+</div>
+<h4 class="text-center">Leave a comment or feedback</h4>
+<div class="text-center mt-5" id="responseIndicator">
+</div>
+`;
+
+export { contactFragBodySnippet };
