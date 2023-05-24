8a30b77365393c0f348c7e97be08b8a615bdaace
ellis <lsp1@hotmail.com>
2023-05-24 11:22:31 -0600

    edit vanilla github code link

--- src/components/construction/under-construction-FRAG.js
+++ src/components/construction/under-construction-FRAG.js
@@ -4,7 +4,7 @@ const underConstructionFRAG = `
 <div class="row pb-3">
   <div class="col-lg-3 construction-ul-opacity mx-auto rounded p-2 my-2">
     <p>
-      <a href="https://github.com/ellis8555/CertificatesOnGit/tree/src-branch" class="under-construction-text-color text-decoration-underline fs-5">This frontend github</a>
+      <a href="https://github.com/ellis8555/portfolio" class="under-construction-text-color text-decoration-underline fs-5">This frontend github</a>
     </p>
     <div class="row d-flex">
       <p class="text-center">Front-end</p>
