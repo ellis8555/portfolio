1a597654b31ffaf8c1438b3617f92fd362c04007
ellis <lsp1@hotmail.com>
2023-05-29 11:11:02 -0600

    check width for proper contact nav hover

--- src/components/navbar/navbar-methods/links-injector.js
+++ src/components/navbar/navbar-methods/links-injector.js
@@ -12,7 +12,11 @@ const createLinkItems = (...linkItems) => {
     li.classList.add("ps-3", "px-md-1", "py-2", "nav-item", "rounded");
     // align right login/sign up
     if (item.marginAuto) {
-      li.classList.add(`${item.marginAuto}`);
+      // checks on smaller screens so contact item will be full width
+      const windowWidth = window.innerWidth;
+      if (windowWidth > 767) {
+        li.classList.add(`${item.marginAuto}`);
+      }
     }
     li.setAttribute("data-name", `${item.elementName}`);
 
