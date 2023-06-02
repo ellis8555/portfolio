44fea0c511eac7461b2a6b97eda4e07ef17c1f39
ellis <lsp1@hotmail.com>
2023-06-02 10:05:03 -0600

    add navbarLayoutOnResize to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./main.scss";
 import * as bootstrap from "bootstrap";
 import { navbarElement, homeElement } from "./components/allElements";
-import { pageStartup } from "./utilities/allUtilities";
+import { pageStartup, navbarLayoutOnResize } from "./utilities/allUtilities";
 
 window.addEventListener("DOMContentLoaded", () => {
   // the root div in index.html for content to be injected
@@ -9,4 +9,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // intial page start up to load the navbar and home elements
   pageStartup(root, navbarElement, homeElement);
+
+  // ensure correct navbar layout on window resize
+  navbarLayoutOnResize();
 });
