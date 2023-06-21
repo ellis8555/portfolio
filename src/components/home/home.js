a9c05ede4d5eb1df61644140bf1582b7d2120488
ellis <lsp1@hotmail.com>
2023-06-21 12:08:40 -0600

    home update message for docker image

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,7 +4,10 @@ import { homeFRAG } from "./home-FRAG/homeFRAG";
 const index = `<div class="mt-5 px-sm-5">
 <p class="display-5 text-center mb-5">My portfolio</p>
 <div class="rounded card-opacity">
-    <div id="homeContent">${homeFRAG}</div>
+<div>
+<p class="bg-primary text-center rounded">Update: <a class="text-white" href="https://hub.docker.com/r/ellis8555/docker-realms">Docker image</a> created for my java project</p>
+</div>
+<div id="homeContent">${homeFRAG}</div>
 </div>
 </div>`;
 
