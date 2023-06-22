c5d028add11115bc8441713b364772d4720ba863
ellis <lsp1@hotmail.com>
2023-06-22 13:12:39 -0600

    home update message updated for docker images

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,7 +5,7 @@ const index = `<div class="mt-5 px-sm-5">
 <p class="display-5 text-center mb-5">My portfolio</p>
 <div class="rounded card-opacity">
 <div>
-<p class="bg-primary text-center rounded">Update: <a class="text-white" href="https://hub.docker.com/r/ellis8555/docker-realms">Docker image</a> created for my java project</p>
+<p class="bg-primary text-center rounded">Update: <a class="text-white" href="https://hub.docker.com/repositories/ellis8555">Docker images</a> created for my java and python projects</p>
 </div>
 <div id="homeContent">${homeFRAG}</div>
 </div>
