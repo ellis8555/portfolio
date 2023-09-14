9b98ab764061e5c86bad4a7a53e9a5527daf3523
ellis <lsp1@hotmail.com>
2023-09-14 13:25:43 -0600

    add nhl95 frag to personal project frag

--- src/components/projects/project-FRAGS/personal-FRAG.js
+++ src/components/projects/project-FRAGS/personal-FRAG.js
@@ -1,7 +1,10 @@
 import { hax94Project } from "./personal-projects/hax94";
+import { nhl95Project } from "./personal-projects/nhl95";
 
 const personalFRAG = `
 <h4 class="text-center my-5">-- Personal --</h4>
+<!-- nhl95 league -->
+${nhl95Project}
 <!-- hax94 league -->
 ${hax94Project}
 `;
