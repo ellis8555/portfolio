5e1757d515cd710196621bbeeeedd7f7eabe3f3a
ellis <lsp1@hotmail.com>
2024-07-29 07:41:45 -0600

    remove no-cors mode post request

--- src/utilities/user-input/submit-contact-form.js
+++ src/utilities/user-input/submit-contact-form.js
@@ -26,7 +26,6 @@ const getContactMessage = async (e) => {
 
   const response = await fetch(CONTACT_ENDPOINT, {
     method: "POST",
-    mode: 'no-cors',
     headers: {
       "Content-Type": "application/json",
     },
